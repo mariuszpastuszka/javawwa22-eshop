43396263a25873edf56005b4324b0b896b1ccc2c
maniek <mariusz.pastuszka@gmail.com>
2020-06-21 12:38:02 +0200

    [change] - temporary renaming of data.sql file

--- src/main/java/pl/sda/javawwa22project/entity/Item.java
+++ src/main/java/pl/sda/javawwa22project/entity/Item.java
@@ -1,5 +1,6 @@
 package pl.sda.javawwa22project.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,6 +14,7 @@ import java.math.BigDecimal;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 @Builder
 @Entity
 @Table(name = "SHOP_ITEMS")
